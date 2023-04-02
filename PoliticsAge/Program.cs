29fa9086042eadbd017798ec5843e14bd336e1fd
User <User@DESKTOP-ROOTJT0>
2023-04-02 22:42:39 +0200

    Add project files.

---
+++ PoliticsAge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PoliticsAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me your age, please.");
+            string age = Console.ReadLine();
+            int chosenAge = 0;
+            Int32.TryParse(age, out chosenAge);
+            
+            if (chosenAge >= 21) 
+            {
+                Console.WriteLine("You are ready to become a politics!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you are to young yet!");
+            }
+        }
+    }
+
+}
